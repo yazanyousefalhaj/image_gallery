de804b333614daad9cf7c082fbe86f2642467043
Yazan Yousefalhaj <yazanyousefalhaj@gmail.com>
2021-08-22 17:45:10 +0300

    format

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
 window.addEventListener('DOMContentLoaded', async (event) => {
-	const CHUNK_SIZE = 20;
-	let selectedCount = 0;
+    const CHUNK_SIZE = 20;
+    let selectedCount = 0;
+    let chunk = 0;
 
     const main = document.querySelector("main");
-	const activeCountElement = document.createElement("p");
+    const activeCountElement = document.createElement("p");
 
-	const imageGalleryElement = document.createElement("div");
+    const imageGalleryElement = document.createElement("div");
     imageGalleryElement.classList.add("image-gallery");
 
     const buttonWrapper = document.createElement("div");
@@ -20,18 +21,18 @@ window.addEventListener('DOMContentLoaded', async (event) => {
         main.appendChild(e);
     })
 
-	const onImageSelected = (id) => {
-		const imageEl = document.querySelector(`#image-${id} .card-body`);
-		const wasAdded = imageEl.classList.toggle("active");
-		selectedCount += wasAdded ? 1 : -1;
-		updateActiveCount(selectedCount);
-	}
+    const onImageSelected = (id) => {
+        const imageEl = document.querySelector(`#image-${id} .card-body`);
+        const wasAdded = imageEl.classList.toggle("active");
+        selectedCount += wasAdded ? 1 : -1;
+        updateActiveCount(selectedCount);
+    }
 
-	const renderImage = (imageAttr) => {
-		const wrapper = document.createElement("div");
-		wrapper.classList.add("card")
-		wrapper.id = `image-${imageAttr.id}`
-		wrapper.innerHTML = `
+    const renderImage = (imageAttr) => {
+        const wrapper = document.createElement("div");
+        wrapper.classList.add("card")
+        wrapper.id = `image-${imageAttr.id}`
+        wrapper.innerHTML = `
 			<img src="${imageAttr.thumbnailUrl}" class="card-img" alt="..." />
 			<div class="card-body">
 				<h5 class="card-title">${imageAttr.title}</h5>
@@ -42,39 +43,35 @@ window.addEventListener('DOMContentLoaded', async (event) => {
 				<button href="#!" class="btn">Select</button>
 			</div>
 		`
-		wrapper.querySelector("button").addEventListener("click", (e) => {
-			onImageSelected(imageAttr.id);
-            e.target.innerHTML = e.target.innerHTML == "Select"? "Unselect": "Select"
+        wrapper.querySelector("button").addEventListener("click", (e) => {
+            onImageSelected(imageAttr.id);
+            e.target.innerHTML = e.target.innerHTML == "Select" ? "Unselect" : "Select"
         });
-		return wrapper;
-	};
+        return wrapper;
+    };
 
 
-	const loadChunk = (imagesArr, chunkIndex) => {
-		let template = []
-		const slice = imagesArr.slice(chunkIndex * CHUNK_SIZE, (chunkIndex + 1) * CHUNK_SIZE)
-		for (let image of slice) {
-			template.push(renderImage(image));
-		}
-		console.log(template)
-		return template;
-	}
+    const loadChunk = (imagesArr, chunkIndex) => {
+        let template = []
+        const slice = imagesArr.slice(chunkIndex * CHUNK_SIZE, (chunkIndex + 1) * CHUNK_SIZE)
+        for (let image of slice) {
+            template.push(renderImage(image));
+        }
+        return template;
+    }
 
-	const updateActiveCount = (n) => {
-		if (activeCountElement) {
-			activeCountElement.innerText = `Active elements: ${n}`
-		}
-	}
+    const updateActiveCount = (n) => {
+        if (activeCountElement) {
+            activeCountElement.innerText = `Active elements: ${n}`;
+        }
+    }
 
-	const imagesJson = await fetch("https://jsonplaceholder.typicode.com/photos").then(r => r.json());
-
-	console.log(imagesJson);
-
-	let chunk = 0;
+    const imagesRes = await fetch("https://jsonplaceholder.typicode.com/photos");
+    const imagesJson = await imagesRes.json()
 
     loadChunk(imagesJson, chunk).forEach(e => imageGalleryElement.appendChild(e));
 
-	updateActiveCount(selectedCount);
+    updateActiveCount(selectedCount);
 
     loadMoreButton.addEventListener("click", (e) => {
         chunk++;
